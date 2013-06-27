d8c9b59ffc4732042b185d9a917b1a424a133314
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-06-26 20:55:25 -0700

    uhhhh

--- url_2/app/controllers/index.rb
+++ url_2/app/controllers/index.rb
@@ -6,14 +6,12 @@ post '/urls' do
   path = params[:original_url]
   @url = Url.new(:original => path)
   if @url.save
-    @last_url = Url.last
+    @url = @url
     @shortened = SecureRandom.hex(3)
-    @last_url.update_attributes(:shortened => @shortened)
+    @url.update_attributes(:shortened => @shortened)
     erb :_shortened
-    #return partial with original, shortened, etc. & route to view
   else 
     erb :_error
-    #return error partial
   end 
 end
 
@@ -21,3 +19,5 @@ get '/:short_url' do
  intermediate = Url.find_by_shortened(params[:short_url])
  redirect intermediate.original
 end
+
+
